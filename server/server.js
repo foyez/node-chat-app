98bb79773ad936a184c240d6da1fc6c228f17290
Foyez <foyezar@gmail.com>
2018-11-24 20:06:58 +0600

    Add connect and disconnect event handler of socket.io

--- server/server.js
+++ server/server.js
@@ -1,18 +1,30 @@
 require('./config/config')
 require('./db/mongoose')
 
-const path = require('path')
-const publicPath = path.join(__dirname, '../public')
-const express = require('express')
-const { ObjectID } = require('mongodb')
+const path          = require('path'),
+      http          = require('http'),
+      express       = require('express'),
+      { ObjectID }  = require('mongodb'),
+      socketIO      = require('socket.io')
 
-const app = express()
+const publicPath = path.join(__dirname, '../public')
 const port = process.env.PORT
+const app = express()
+const server = http.createServer(app)
+const io = socketIO(server)
 
 app.use(express.static(publicPath))
+
+io.on('connection', (socket) => {
+  console.log('New user connected')
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected')
+  })
+})
 
 // app.get('/', (req, res) => {
 //   res.send('Hello')
 // })
 
-app.listen(port, () => console.log(`Server started on ${port}`))
+server.listen(port, () => console.log(`Server started on ${port}`))
